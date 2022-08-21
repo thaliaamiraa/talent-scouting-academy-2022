286fac9187ec1d66555d64e42677a552c184e259
thaliaamiraa <thalia02amira@gmail.com>
2022-08-21 18:27:48 +0700

    update

---
+++ Handsout6_javaquery/Praktikum/variabel.php
@@ -0,0 +1,16 @@
+<?php
+
+// membuat variabel baru
+$nama_barang = "Minyak Goreng";
+$harga = 15000;
+
+// menampilkan isi variabel
+echo "Ibu membeli $nama_barang seharga Rp $harga";
+
+$judul = "Belajar PHP dari nol sampai expert ";
+echo " Judul artikel: $judul";
+
+$judul ="Tutorial PHP untuk Pemula";
+echo 'Judul artikel: '.$judul;
+
+?>
